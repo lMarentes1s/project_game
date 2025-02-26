e01c35407e05abcbe29e306581b660d1ce821e55
Marentesis <lmlopezma@ut.edu.co>
2025-02-26 09:37:19 -0500

    Parametros con exepciones

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+
+import java.util.HashMap;
+import java.util.Map;
+
+    public class Main {
+        public static void main(String[] args) {
+            if (args.length > 1) {
+
+                Map<String, String> paramMap = new HashMap<>();
+
+                for (String param : args) {
+                    String[] keyValue = param.split("=");
+                    paramMap.put(keyValue[0], keyValue[1]);
+
+                }
+
+
+                String algorithm = paramMap.get("a");
+                String listType = paramMap.get("t");
+                String order = paramMap.get("c");
+                String piece = paramMap.get("r");
+                StringBuilder time = new StringBuilder(paramMap.get("s"));
+                int newTime = Integer.parseInt(time.toString());
+
+                User user = new User(algorithm, listType, order, piece, newTime);
+
+                System.out.println(user);
+            } else {
+                System.out.println("Please provide the parameters in the correct format.");
+            }
+
+
+
+
+        }
+    }
+
+
+
---
+++ src/User.java
@@ -0,0 +1,111 @@
+public class User {
+    private String Algorithm;
+    private String ListType;
+    private String order;
+    private String piece;
+    private int time;
+
+
+    private String[] validAlgorithm = {"b", "q", "m"};  // Bubble, Quick, Merge
+    private String[] validListType = {"n", "c"};  // Numeric, Character
+    private String[] validOrder = {"b", "w"};  // Black,white
+    private String[] validPiece = {"1", "2", "4", "8", "10", "16"};  // 1,2,4,8,10,16
+
+    public User(String Algorithm, String ListType, String order, String piece, int time) {
+        setAlgorithm(Algorithm);
+        setListType(ListType);
+        setOrder(order);
+        setPiece(piece);
+        setTime(time);
+    }
+
+    private boolean isValid(String value, String[] validValues) {
+        for (String valid : validValues) {
+            if (valid.equals(value)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getAlgorithm() {
+        return Algorithm;
+    }
+
+    public void setAlgorithm(String algorithm) {
+        if (isValid(algorithm, validAlgorithm)) {
+            Algorithm = algorithm;
+        } else {
+            throw new IllegalArgumentException("Invalid algorithm");
+        }
+
+    }
+
+    public String getListType() {
+        return ListType;
+    }
+
+    public void setListType(String listType) {
+        if (isValid(listType, validListType)) {
+            ListType = listType;
+        } else {
+            throw new IllegalArgumentException("Invalid list type");
+        }
+    }
+
+    public String getOrder() {
+        return order;
+    }
+
+    public void setOrder(String order) {
+        if (isValid(order, validOrder)) {
+            this.order = order;
+        } else {
+            throw new IllegalArgumentException("Invalid order");
+        }
+    }
+
+    public String getPiece() {
+        return piece;
+    }
+
+    public void setPiece(String piece) {
+        if (isValid(piece, validPiece)) {
+            this.piece = piece;
+        } else {
+            throw new IllegalArgumentException("Invalid piece");
+        }
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        if (time > 100 && time < 1000) {
+            this.time = time;
+        } else {
+            throw new IllegalArgumentException("Invalid time");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "algoritmo='" + Algorithm + '\'' +
+                ", tipo='" + ListType + '\'' +
+                ", color='" + order + '\'' +
+                ", r=" + piece +
+                ", s=" + time +
+                '}';
+    }
+}
+
+
+
+
+
+
+
+
+
